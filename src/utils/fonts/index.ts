a063de63241c7c7250c5586848093f844cb381bd
Rhzslya <rhzslyaaa@gmail.com>
2025-04-12 19:52:58 +0700

    first commit

---
+++ src/utils/fonts/index.ts
@@ -0,0 +1,21 @@
+import { Poppins, Lora, Montserrat, Inter } from "next/font/google";
+
+export const poppins = Poppins({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+});
+
+export const lora = Lora({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const montserrat = Montserrat({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const inter = Inter({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
